c2082d676e73e3a2905c96a48b7d2e96c9e7ca93
TristanWong <tw949561391@icloud.com>
2018-09-07 17:08:43 +0800

    publish

--- app/middleware/boom.js
+++ app/middleware/boom.js
@@ -1,15 +1,15 @@
 'use strict';
-
 module.exports = () => {
     return async (ctx, next) => {
         try {
-            await next();
+            await
+                next();
         } catch (error) {
             if (!ctx.Boom.isBoom(error)) {
-                throw ctx.Boom.boomify(error);
+                throw ctx.Boom.boomify(error, {statusCode: e.statusCode || e.status || 500});
             } else {
                 throw error;
             }
         }
     }
-}
+};
